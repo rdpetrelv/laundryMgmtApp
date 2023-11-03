af79613d31a326ab480886a204af8e14b6b1f435
Fatima Ezzahrae GHAOUI <fatighaoui19@gmail.com>
2023-11-03 15:56:24 -0400

    About-us page

---
+++ compile_cssf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+if [ -x "$(command -v sass)" ]; then
+  SASS_EXEC=sass
+fi
+
+
+if [ -z "$SASS_EXEC" ]; then
+  echo "You need to have SASS installed"
+  exit 1
+fi
+
+CHOKIDAR_USEPOLLING=true sass --watch integration/stylef.scss:integration/assets/stylesheets/outputf.css
